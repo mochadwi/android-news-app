998662f0d39bfd3c0ff074167a80de053dec3d4d
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2018-03-13 16:01:34 +0700

    [UPDATE] News Source - Prep #04
    - Update using untrusted SSL client

--- app/src/main/java/io/mochadwi/mobilenews/network/RESTGenerator.kt
+++ app/src/main/java/io/mochadwi/mobilenews/network/RESTGenerator.kt
@@ -1,13 +1,16 @@
 package io.mochadwi.mobilenews.network
 
 import io.mochadwi.mobilenews.BuildConfig
+import io.mochadwi.mobilenews.model.news_source.UnsafeOkHttpClient
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.security.cert.CertificateException
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.*
 
 /**
  * Created by mochadwi on 3/13/18.
@@ -32,6 +35,7 @@ class RESTGenerator {
                     .connectTimeout(60, TimeUnit.SECONDS)
                     .writeTimeout(60, TimeUnit.SECONDS)
 
+
             okHttpClient.addInterceptor(logging)
 
             return Retrofit.Builder()
@@ -39,7 +43,7 @@ class RESTGenerator {
                     .addConverterFactory(GsonConverterFactory.create())
                     .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .baseUrl(url)
-                    .client(okHttpClient.build())
+                    .client(UnsafeOkHttpClient.getUnsafeOkHttpClient())
                     .build()
         }
 
