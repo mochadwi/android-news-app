b941b4a0406a0982abf227b157f3ba0c8aec8175
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2018-03-15 09:19:59 +0700

    [ADD] Realm Library

---
+++ app/src/main/java/io/mochadwi/mobilenews/MainApplication.kt
@@ -0,0 +1,32 @@
+package io.mochadwi.mobilenews
+
+import android.app.Application
+import android.support.multidex.MultiDexApplication
+import io.mochadwi.mobilenews.util.RealmMigrations
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+/**
+ * Created by mochadwi on 3/15/18.
+ */
+class MainApplication : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+
+        val configuration = RealmConfiguration.Builder()
+                .name("mobilenews.realm")
+                .schemaVersion(1)
+                .migration(RealmMigrations())
+                .build()
+
+        Realm.setDefaultConfiguration(configuration)
+        Realm.getInstance(configuration)
+    }
+
+    override fun onTerminate() {
+        Realm.getDefaultInstance().close()
+        super.onTerminate()
+    }
+}
