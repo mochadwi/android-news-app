f8756e5e2085bdc3e9c2ce6abe478397ceaf6683
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2018-04-02 17:26:04 +0700

    [UPDATE] Add transition on activity changes

--- app/src/dev/java/io/mochadwi/mobilenews/dev/splashscreen/SplashScreenActivity.kt
+++ app/src/dev/java/io/mochadwi/mobilenews/dev/splashscreen/SplashScreenActivity.kt
@@ -11,23 +11,8 @@ import io.mochadwi.mobilenews.util.PublicMethods
  */
 class SplashScreenActivity : AppCompatActivity() {
 
-    private var mPresenter: SplashScreenContract.Presenter? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
-        var view: SplashScreenFragment? = supportFragmentManager
-                .findFragmentById(R.id.fragment_layout) as? SplashScreenFragment
-
-        if (view == null) {
-            view = SplashScreenFragment.newInstance()
-
-            PublicMethods.addFragmentToActivity(supportFragmentManager,
-                    view, R.id.fragment_layout)
-        }
-
-        mPresenter = SplashScreenPresenter(view)
-        mPresenter!!.start()
+        startActivity(Intent(this, NewsSourceActivity::class.java))
     }
 }
